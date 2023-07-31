2316c2a6541e368d7f2f15dc9d7194a0f73258e9
Ruth <osukukuruth@gmail.com>
2023-07-31 15:44:24 -0400

    Done Assignment

--- db/migrate/20230731154948_create_doctor.rb
+++
@@ -1,4 +0,0 @@
-class CreateDoctor < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_31_193406) do
+ActiveRecord::Schema.define(version: 2023_07_31_194336) do
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "speciality"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "full_name"
