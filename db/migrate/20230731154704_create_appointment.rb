78f1b917a3aa726a76961ac3e28f2bd71f37623e
Ruth <osukukuruth@gmail.com>
2023-07-31 12:41:52 -0400

    Done Assignment

--- db/migrate/20230731154704_create_appointment.rb
+++ db/migrate/20230731154704_create_appointment.rb
@@ -3,9 +3,16 @@ class CreateAppointments < ActiveRecord::Migration[6.0]
     create_table :appointments do |t|
       t.belongs_to :patient
       t.belongs_to :doctor
-      t.datetime :appointment_time
+      
+      t.string :cancer_service
+      t.date :apt_date
+      t.time :apt_time
+      t.string :appointment_notes
+      t.integer :patient_id
+      t.integer :doctor_id
 
       t.timestamps
+     
     end
   end
 end
